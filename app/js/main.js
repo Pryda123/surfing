51c3501c93cb5276ae97bcfe1f14a51da5bb4cea
Pryda123 <44433921+Pryda123@users.noreply.github.com>
2020-04-02 13:45:29 +0300

    verstka

--- app/js/main.js
+++ app/js/main.js
@@ -27,5 +27,11 @@ $('.slider-map').slick({
     asNavFor: '.serf-slider',
     focusOnSelect: true
 })
+$('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="img/arrows-right.svg" alt="">',
+})
   
 });
